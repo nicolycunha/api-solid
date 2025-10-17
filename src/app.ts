e59484bcee124cfd0e3fb75917d2018475068551
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-10-16 21:50:34 -0300

    feat: create verification per role

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,24 @@ import { gymsRoutes } from './http/controllers/gyms/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { userRoutes } from './http/controllers/users/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 })
+
+app.register(fastifyCookie)
 
 app.register(userRoutes)
 app.register(gymsRoutes)
